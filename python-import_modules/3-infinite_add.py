44fde59069610ee40d7231273fc79879543936a2
Lesego Ledwaba <ledwabalesego199@gmail.com>
2025-07-07 06:13:48 +0000

    Add script to sum command-line arguments

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+import sys
+
 if __name__ == "__main__":
-    import sys
     total = 0
-    for num in sys.argv[1:]:
-        total += int(num)
+    for i in range(1, len(sys.argv)):
+        total += int(sys.argv[i])
     print(total)
